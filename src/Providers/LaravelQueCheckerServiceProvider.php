39ac512beb61f879adf36f93ea5a15c856be6ebb
Admir <admir.alagic@gmail.com>
2020-09-25 09:43:24 +0200

    boot schedule tasks

--- src/Providers/LaravelQueCheckerServiceProvider.php
+++ src/Providers/LaravelQueCheckerServiceProvider.php
@@ -3,12 +3,19 @@
 namespace Omnitask\LaravelQueChecker\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Console\Scheduling\Schedule;
 use Omnitask\LaravelQueChecker\Console\Commands\CheckIsQueWorking;
 use Omnitask\LaravelQueChecker\Console\Commands\CheckQue;
 use Omnitask\LaravelQueChecker\Console\Commands\DeleteOldQueHeartbeats;
 
 class LaravelQueCheckerServiceProvider extends ServiceProvider
 {
+    protected $commands = [
+        CheckIsQueWorking::class,
+        CheckQue::class,
+        DeleteOldQueHeartbeats::class,
+    ];
+
     /**
      * Register services.
      *
@@ -37,6 +44,8 @@ class LaravelQueCheckerServiceProvider extends ServiceProvider
 
         $this->registerCommands();
         $this->registerPublishing();
+        $this->bootSchedule();
+
     }
 
     /**
@@ -59,11 +68,16 @@ class LaravelQueCheckerServiceProvider extends ServiceProvider
 
     private function registerCommands(){
         if ($this->app->runningInConsole()) {
-            $this->commands([
-                CheckIsQueWorking::class,
-                CheckQue::class,
-                DeleteOldQueHeartbeats::class,
-            ]);
+            $this->commands($this->commands);
         }
+    }
+
+    private function bootSchedule(){
+        $this->app->booted(function () {
+            $schedule = $this->app->make(Schedule::class);
+             $schedule->command(CheckQue::class)->cron('1,6,11,16,21,26,31,36,41,46,51,56 * * * *');
+             $schedule->command(CheckIsQueWorking::class)->cron('3,8,13,18,23,28,33,38,43,48,53,58 * * * *');;
+             $schedule->command(DeleteOldQueHeartbeats::class)->dailyAt('03:00');
+        });
     }
 }
